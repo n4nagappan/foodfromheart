54e86c8f63b656fe5e915657fa517d241b534685
EB <admin@EBs-MacBook-Air.local>
2014-06-08 08:03:33 +0800

    everyy

--- js/render-map-route.js
+++ js/render-map-route.js
@@ -1,3 +1,35 @@
+function getDuration(from, to, mode){
+
+    var service = new google.maps.DistanceMatrixService();
+    service.getDistanceMatrix(
+      {
+        origins: [from],
+        destinations: [to],
+        travelMode: mode,
+        unitSystem: google.maps.UnitSystem.METRIC
+      }, mycallback);
+
+    function mycallback(response, status) {
+      if (status == google.maps.DistanceMatrixStatus.OK) {
+        var origins = response.originAddresses;
+        var destinations = response.destinationAddresses;
+
+        for (var i = 0; i < origins.length; i++) {
+          var results = response.rows[i].elements;
+          for (var j = 0; j < results.length; j++) {
+            var element = results[j];
+            var distance = element.distance.text;
+            var duration = element.duration.text;
+            var from = origins[i];
+            var to = destinations[j];
+            
+            var m = document.getElementById("exptTime");
+            m.innerHTML = duration;
+          }
+        }
+      }
+    }
+}
 
 var directionsService = new google.maps.DirectionsService();
 
@@ -83,6 +115,8 @@ function calcRoute()
       alert("No routing available. Please inform us to verify the address.");
     }
   });
+  
+  getDuration(originAddress, destinationAddress, google.maps.TravelMode[selectedMode]);
 }
 
 google.maps.event.addDomListener(window, 'load', map_inititalize);
