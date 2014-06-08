8bad08d241e39aaa179647860a99871fdeb6d9ed
elayabharath <elayabharath.elango@autodesk.com>
2014-06-08 14:17:13 +0800

    Add

--- js/notifications.js
+++ js/notifications.js
@@ -3,6 +3,6 @@ var socket = io('http://ffh.cloudapp.net');
   socket.on('request', function (data) {
     console.log(data);
     $("#notification").text(data.length);
-      localStorage.setItem("dataRequests", data);
+      localStorage.setItem("dataRequests", JSON.stringify(data));
     //socket.emit('accept', { acceptData: 'TestData' });
   });
