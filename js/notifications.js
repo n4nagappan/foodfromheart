09c48c34e57597c28e6c69f6e6973988f9c83ebc
Nagappan Nachiappan <n4nagappan@gmail.com>
2014-06-08 09:06:09 +0800

    bug fix

--- js/notifications.js
+++ js/notifications.js
@@ -1,5 +1,5 @@
  var socket = io('http://ffh.cloudapp.net');
-  socket.on('news', function (data) {
+  socket.on('request', function (data) {
     console.log(data);
     socket.emit('myEvent', { my: 'data' });
   });
