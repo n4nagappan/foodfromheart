e53f222d537cea05d38f44991dbdaaccdde0292b
Nagappan Nachiappan <n4nagappan@gmail.com>
2014-06-08 10:16:50 +0800

    notif

--- js/notifications.js
+++ js/notifications.js
@@ -1,5 +1,5 @@
  var socket = io('http://ffh.cloudapp.net');
   socket.on('request', function (data) {
     console.log(data);
-    socket.emit('myEvent', { my: 'data' });
+    socket.emit('accept', { acceptData: 'TestData' });
   });
